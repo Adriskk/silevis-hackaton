afb21eb60e5ebd811e0eeabcea45f6d7e9817f76
Adriskk <65545676+Adriskk@users.noreply.github.com>
2022-12-02 18:48:40 +0100

    Initialization of the project structure and starting state

---
+++ src/layouts/Navigation.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Navigation: FC = () => {
+  return <nav>nav</nav>;
+};
+
+export default Navigation;
---
+++ src/views/Dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Dashboard: FC = () => {
+  return <section>section</section>;
+};
+
+export default Dashboard;
