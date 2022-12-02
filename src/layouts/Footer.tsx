871fc0a28ab48859eacda611b33c75d9abedc2a9
Jakub <zkuba94@gmail.com>
2022-12-03 00:02:22 +0100

    added styles to footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,7 +1,11 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
 const Footer: FC = () => {
-  return <footer>footer</footer>;
+	return (
+		<footer className='flex bg-indigo-700 justify-content-center align-items-center p-3'>
+			©2022 All rights reserved. Designed and Created by 4devs.
+		</footer>
+	);
 };
 
 export default Footer;
