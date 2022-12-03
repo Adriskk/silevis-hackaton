c9ff5d4eb149a97efd3298d0ccf3689b546761fb
Jakub <zkuba94@gmail.com>
2022-12-03 02:25:40 +0100

    added navigation

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,7 +1,22 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
+
+import { TabMenu } from 'primereact/tabmenu';
+import { Button } from 'primereact/button';
 
 const Navigation: FC = () => {
-  return <nav>nav</nav>;
+	const items = [
+		{ label: 'Players', icon: 'pi pi-fw pi-users' },
+		{ label: 'Profile', icon: 'pi pi-fw pi-user-edit' },
+		{ label: 'Settings', icon: 'pi pi-fw pi-cog' },
+	];
+
+	return (
+		<nav className='flex justify-content-center gap-6 p-5'>
+			<span className='flex align-items-center text-xl'>PlayersMarket</span>
+			<TabMenu model={items} />
+			<Button label='Register' icon='pi pi-arrow-right' iconPos='right' />
+		</nav>
+	);
 };
 
 export default Navigation;
